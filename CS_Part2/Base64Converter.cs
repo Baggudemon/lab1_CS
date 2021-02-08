7689a37413a7d79b202bddc148d4e8e853795984
Dmitriy Petrykovych <dimon21341@gmail.com>
2021-02-08 12:30:51 +0200

    algorithm refactoring

--- CS_Part2/Base64Converter.cs
+++ CS_Part2/Base64Converter.cs
@@ -8,151 +8,41 @@ namespace CS_Part2
 {
     public class Base64Converter
     {
-        const string Base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-        public static string convert(string text)
+        const string Base64Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+        public static string convert(byte[] byteMass)
         {
-            string stringBase64 = "";
-            int Padding;
-            int countOfIterator = 0;
-            string stringBuffer = "";
-            char[] arrayBuffer = new char[6];
-
-
-            List<char[]> listOfBytes = ASCIIConverter.convert(text);
-            List<char[]> listOfByteBase64 = new List<char[]>();
-
-
-            int j = 0;
-            int numberOfCase = 0;
-
-            foreach (char[] array in listOfBytes)
+            string final = "";
+            int Base = byteMass.Length % 3;
+            for (int i = 0; i < byteMass.Length - Base; i = i + 3)
             {
-                for (int i = 0; i < array.Length; i++)
-                {
-                    arrayBuffer[j] = array[i];
+                byte One = byteMass[i];
+                byte Two = byteMass[i + 1];
+                byte Three = byteMass[i + 2];
 
-                    if (j == 5)
-                    {
-                        if (!((Padding = (array.Length - i + 1)) % 3 == 0))
-                            if (Padding % 3 == 1)
-                                numberOfCase = 1;
-                            else
-                                if (Padding % 3 == 2)
-                                numberOfCase = 2;
+                final += Base64Alphabet[One >> 2];
 
-                        listOfByteBase64.Add(arrayBuffer);
-                        arrayBuffer = new char[6];
-                        j = 0;
-                        if (listOfBytes.Count - 1 == countOfIterator)
-                        {
-                            int counterOfZeros = 0;
-                            for (i += 1; i < array.Length; i++)
-                            {
-                                counterOfZeros++;
-                                arrayBuffer[j] = array[i];
-                                j++;
-                            }
-                            for (; j < arrayBuffer.Length; j++)
-                            {
-                                arrayBuffer[j] = '0';
-                            }
-                            listOfByteBase64.Add(arrayBuffer);
-                            break;
-                        }
-                        continue;
-                    }
-                    j++;
-                }
-                countOfIterator++;
+                final += Base64Alphabet[((One & 3) << 4) | (Two >> 4)];
+
+                final += Base64Alphabet[((Two & 15) << 2) | (Three >> 6)];
+
+                final += Base64Alphabet[Three & 63];
             }
-            for (int i = 0; i < listOfByteBase64.Count; i++)
+            if (Base == 1)
             {
-                stringBuffer = "";
-                foreach (char number in listOfByteBase64[i])
-                {
-                    stringBuffer += number;
-                }
-                stringBase64 += Base64[Convert.ToInt32(stringBuffer, 2)];
+                byte One = byteMass[byteMass.Length - 2];
+                final += Base64Alphabet[One >> 2];
+                final += Base64Alphabet[((One & 3) << 4)];
+                final += "==";
             }
-            if (numberOfCase == 1)
-                stringBase64 += "==";
-            else
-                 if (numberOfCase == 2)
-                stringBase64 += "=";
-
-            return stringBase64;
-        }
-        public static string convert(List<char[]> ASCIIText)
-        {
-            string stringBase64 = "";
-            string stringBuffer = "";
-            int Padding;
-            int countOfIterator = 0;
-            char[] arrayBuffer = new char[6]; 
-
-            //List<char[]> listOfBytes = AsciConverter(text);
-            List<char[]> listOfByteBase64 = new List<char[]>();
-            int j = 0;
-
-            int numberOfCase = 0;
-         
-
-            foreach (char[] array in ASCIIText)
+            if (Base == 2)
             {
-                for (int i = 0; i < array.Length; i++)
-                {
-                    arrayBuffer[j] = array[i];
-
-                    if (j == 5)
-                    {
-                        if (!((Padding = (array.Length - i + 1)) % 3 == 0))
-                            if (Padding % 3 == 1)
-                                numberOfCase = 1;
-                            else
-                                if (Padding % 3 == 2)
-                                numberOfCase = 2;
-
-                        listOfByteBase64.Add(arrayBuffer);
-                        arrayBuffer = new char[6];
-                        j = 0;
-                        if (ASCIIText.Count - 1 == countOfIterator)
-                        {
-                            int counterOfZeros = 0;
-                            for (i += 1; i < array.Length; i++)
-                            {
-                                counterOfZeros++;
-                                arrayBuffer[j] = array[i];
-                                j++;
-                            }
-                            for (; j < arrayBuffer.Length; j++)
-                            {
-                                arrayBuffer[j] = '0';
-                            }
-                            listOfByteBase64.Add(arrayBuffer);
-                            break;
-                        }
-                        continue;
-                    }
-                    j++;
-                }
-                countOfIterator++;
+                byte One = byteMass[byteMass.Length - 2];
+                byte Two = byteMass[byteMass.Length - 1];
+                final += Base64Alphabet[One >> 2];
+                final += Base64Alphabet[((One & 3) << 4) | (Two >> 4)];
+                final += "=";
             }
-            for (int i = 0; i < listOfByteBase64.Count; i++)
-            {
-                stringBuffer = "";
-                foreach (char number in listOfByteBase64[i])
-                {
-                    stringBuffer += number;
-                }
-                stringBase64 += Base64[Convert.ToInt32(stringBuffer, 2)];
-            }
-            if (numberOfCase == 1)
-                stringBase64 += "==";
-            else
-                 if (numberOfCase == 2)
-                stringBase64 += "=";
-
-            return stringBase64;
+            return final;
         }
     }
 }
